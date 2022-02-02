b99ede687ea818d645e47ff1915c57eac29aa0ac
Laura Pareja-Prieto <lpareja@drury.edu>
2022-02-02 12:21:30 -0600

    step 4 done

--- pseudo_rand_genera/spectral_test/knuth.py
+++ pseudo_rand_genera/spectral_test/knuth.py
@@ -1,5 +1,6 @@
 from cmath import sqrt
 from math import floor
+import math
 import numlib as nl
 import numpy as np
 
@@ -17,7 +18,7 @@ c = R(12121212121) # an element of R
 state = 10**10+1
 
 # let's use knuth to compute v2
-def knuth():
+def knuth(T):
 
     # S1) initialize values
     t = 2
@@ -60,12 +61,48 @@ def knuth():
 
     #print(U,V)
 
+    # S4) advance step
+    while (t<T):
+        t = t+1
+        r = int(R(a*r))
+
+        # enlarge U and V to be txt mtx´s
+        Ut = [-r] #append first element of the row for U(-r)
+        for i in range(1,t-1): Ut.append(0) #middle rows elements are 0's
+        Ut.append(1) #last element row is a 1
+
+        # add column and row
+        U = np.pad(U, ((0,0),(0,1)), mode='constant', constant_values=0)
+        U = np.vstack([U,Ut])
+
+        Vt = [] #new row for V
+        V = np.pad(V, ((0,0),(0,1)), mode='constant', constant_values=0)
+        for i in range(0,t-1): Vt.append(0) #all element except last one are 0's
+        Vt.append(m) #last element for new row is m
+        V = np.vstack([V,Vt])
+        
+        for i in range(0,t-1): #is this working right?
+            q = math.floor(V[i][0]/m)
+            V[i][t-1] = (V[i][0]*r) - q*m
+            U[t-1] = U[t-1] + (q * U[:,i])
+
+        s = min(s, np.dot(U[t-1],U[t-1])) #min(s, Ut*Ut) 
+        k = t #last index transformation
+        j = 1 #denotes current row index
+
+
+            
+        print("U: ", U)
+        print("V: ", V)
+        print("Current s: ", s)
+
+    # S5) 
     
 
-    return (s**0.5)
     
     
-print(knuth())
+    
+print(knuth(3))
 
 # Not done yet, comparison part
 #comparison = 2^(int(30/2))
