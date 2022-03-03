20aef3e2f983df8ef6daff05e97751d0c573d4c7
Laura Pareja-Prieto <lpareja@drury.edu>
2022-03-03 13:48:17 -0600

    notes

---
+++ Part IV/gamal_encript/gamalEncript.py
@@ -0,0 +1,27 @@
+#Ambient key = {G,g} of order n
+#   everyonr in the planet has g
+
+#you: 
+#   1) create random d in Z/n --> private key
+#   2) computer (h) h = g^d --> public key
+
+#Athena:
+#   1) wants to encrypt m in G
+#   2) choose r randomly in Z/n
+#   3) computes c1 = g^r --> part of a msg? element of G
+#   4) computes c2 = (h^r)*m element of G
+#   5) sends {c1,c2} --> encripted msg (chipertext) divide in two
+#   6) r can be discarded after this is done
+
+#   do to use {c1,c2} to obtain m (m is the msg) non encripted
+
+#you (after receiving {c1,c2})
+#   1) compute c2*c1^(-d) --> m
+#   2) this is the same as --> (h^(r) * m) * ( g^(r))^(-d)
+#                               g^(d*r) * m * g^(r* -d)
+#                               m
+#   (g^(-d)) = g^(-1) * d
+
+
+# take G = (Z/p)*
+# then get G to be an eleptic curve -
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 > Go to the wiki for references
+
+Key points: 
+-   difference between symetric key and asymetric keys
+-   Hybrid encrypation system: Combination of symetric and asymetric encriptation
