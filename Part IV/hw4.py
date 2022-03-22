0f47b1d175847b340bd320974989bed7235cc8a8
Marina Amorin <mamorim@drury.edu>
2022-03-22 13:27:24 -0500

    added test

--- Part IV/hw4.py
+++ Part IV/hw4.py
@@ -3,33 +3,42 @@
 from re import L
 
 import numlib as nl
-z = 8
-myMod = 29
-l= 17
-a,r,s = nl.xgcd(z,myMod)
-R_1 = nl.Zmod(a)
-R_2 = nl.Zmod(myMod)
 
-#if (R_1(z)==0):
+R_2 = nl.Zmod(2131233)
 
-b=l/a
-solution = r*int(b)
 
-print(solution)
 
 test = 8*187
-print(R_2(test))
-test2= 1234* -47590804135
-print()
+
+
+#print(R_2(test))
+test2= 1234 * -47590804135
+print(R_2(test2))
 
 def SolveMult(z,myMod,l):
     a,r,s = nl.xgcd(z,myMod)
     R_1 = nl.Zmod(a)
-    R_2 = nl.Zmod(myMod)
-    b=l/a
-    solution = r*int(b) 
-    return(solution)
+    if (R_1(a)==0):
+        b=l/a
+        solution = r*int(b) 
+        return solution
+    else:
+        return False
 
 print("Ex 1",SolveMult(8,29,17))
 print("Ex 2",SolveMult(1234,2131233,98765))
+
+
+def testMult(z,myMod,l):
+    mult = SolveMult(z,myMod,l)
+    mult *= z
+    R_test =  nl.Zmod(myMod)
+    if (R_test(mult)==l):
+        return True
+    else:
+        return False
+
+print(testMult(8,29,17))
+print(testMult(1234,2131233,98765))
+        
 
