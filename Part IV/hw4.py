36356edc8311c1df26bc4d90e7769f496abd1572
Laura Pareja-Prieto <lpareja@drury.edu>
2022-03-23 18:13:14 -0500

    3&4

--- Part IV/hw4.py
+++ Part IV/hw4.py
@@ -43,20 +43,25 @@ print(testAdd(1234,2131233,98765))
 
 # Take equationz^x = l mod myMod
 # z=5 l=78 mymod=257
-def SolveMultG(z,myMod,l):
-    F = nl.Zmodp(myMod)
-    multOrder = nl.mulorder(F(z))
-    for x in range(myMod):
-        if (multOrder ** x ==l):
-            return x
-    #return False
 
+def solveMulgG(n,a,p):
+    F = nl.Zmodp(p)
+    g = nl.mulorder(F(n))
+    #print(" generator: ", g)
+    for x in range (p): 
+        print(x)       
+        if(F(pow(n,x)) == a):
+            print(" x: ", x)
 
-
-
-
-print(SolveMultG(5,257,78))
-print(SolveMultG(15551,8675309,1357))
+#exercise 3
+print("exercise 3: ")
+#solveMulgG(5,78,257)
+#exercise 4
+print("exercise 4: ")
+solveMulgG(15551,1357,4337654)
+#Multiplicative Marina&Laura 
+#solveMulgG(13,4,89)
+#olveMulgG(197, 23, 3001)
  
 # y^2 = x^3 +ax +b mod Mymod
 def SolveEll(a,b,myMod,p1_x,p1_y,p2_x,p2_y,l):
@@ -66,6 +71,27 @@ def SolveEll(a,b,myMod,p1_x,p1_y,p2_x,p2_y,l):
         print(pt)
     E(p1_x,p1_y)
     E(p2_x,p2_y)
+
+
+p = 19
+F = nl.Zmodp(p)
+q = F(7)
+r = F(10)
+P = (2,2)
+Q = (3,4)
+def solveEllip(p,q,r,F,P,Q):
+    E = nl.EllCurve(q,r, debug = True) # create elliptic curve
+    len(list(nl.affine(E)))
+    E(4,19)*0 #magic number
+
+    for pt in nl.affine(E):   #print all pts on the Elliptic Curve
+        print(pt) 
+        print(pt)**(-1)
+
+ 
+
+    #check if points are in the elliptic curve
+    
 
         
 
